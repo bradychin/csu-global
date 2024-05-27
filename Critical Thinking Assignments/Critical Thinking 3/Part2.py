f45372ace57fa60c19565a8faa2e8c5b4ae42c5a
Brady <bradychin11@icloud.com>
2024-05-27 18:28:42 -0500

    Update Part2.py

--- Critical Thinking Assignments/Critical Thinking 3/Part2.py
+++ Critical Thinking Assignments/Critical Thinking 3/Part2.py
@@ -1,5 +1,13 @@
 def main():
-    pass
+    time_now = input('\nEnter the current time (24hr): ')
+    wait = input('Enter how many hours to wait for the alarm: ')
+    alarm_time = calculate_time(time_now, wait)
+
+    print(f'\nYour alarm will ring at {alarm_time}:00.\n')
+
+def calculate_time(current_time, duration):
+    parse = divmod(int(duration), 12)
+    return int(current_time) + parse[1]
 
 if __name__ == '__main__':
     main()
