7b4cc0b6cd5505b0781e907e465f88794e0690a5
Brady <bradychin11@icloud.com>
2024-05-27 18:03:46 -0500

    Update Part1.py

--- Critical Thinking Assignments/Critical Thinking 3/Part1.py
+++ Critical Thinking Assignments/Critical Thinking 3/Part1.py
@@ -1,5 +1,16 @@
+from decimal import *
+
 def main():
-    pass
+    charge = input('\nEnter the cost of the food: $')
+    tip = Decimal('0.18')
+    sales_tax = Decimal('0.07')
+    
+    amount_with_tip = Decimal(charge) * tip
+    amount_with_tax = Decimal(charge) * sales_tax
+
+    print(f'\n18% tip: ${amount_with_tip}')
+    print(f'7% sales tax: ${amount_with_tax}')
+    print(f'Total price: ${Decimal(charge) + amount_with_tip + amount_with_tax}\n')
 
 if __name__ == '__main__':
     main()
