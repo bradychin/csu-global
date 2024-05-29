3e554cb7fa3441add00ed92d78b9ce383ccf5479
Brady <bradychin11@icloud.com>
2024-05-29 15:27:00 -0500

    Updated Files
    Added error handling to Part1.py. Added error handling and fixed bugs in Part2.py. Created Pseudocode for Part1.py and Part2.py

--- Critical Thinking Assignments/Critical Thinking 3/Part1.py
+++ Critical Thinking Assignments/Critical Thinking 3/Part1.py
@@ -1,16 +1,27 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
 from decimal import *
 
 def main():
-    charge = input('\nEnter the cost of the food: $')
+    while True:
+        try:
+            charge = Decimal(input('\nEnter the cost of the food: $'))
+            if charge < 0:
+                print('Food isn\'t free!')
+            else:
+                break
+        except Exception:
+            print('Please enter a number.')
+
     tip = Decimal('0.18')
     sales_tax = Decimal('0.07')
     
-    amount_with_tip = Decimal(charge) * tip
-    amount_with_tax = Decimal(charge) * sales_tax
+    amount_with_tip = charge * tip
+    amount_with_tax = charge * sales_tax
 
     print(f'\n18% tip: ${amount_with_tip}')
     print(f'7% sales tax: ${amount_with_tax}')
-    print(f'Total price: ${Decimal(charge) + amount_with_tip + amount_with_tax}\n')
+    print(f'Total price: ${charge + amount_with_tip + amount_with_tax}\n')
 
 if __name__ == '__main__':
     main()
--- Critical Thinking Assignments/Critical Thinking 3/Part2.py
+++ Critical Thinking Assignments/Critical Thinking 3/Part2.py
@@ -1,9 +1,28 @@
-def main():
-    time_now = input('\nEnter the current time (00hr - 23hr): ')
-    wait = input('Enter how many hours to wait for the alarm: ')
-    alarm_time = calculate_alarm_time(int(time_now), int(wait))
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
 
-    print(f'\nYour alarm will ring at {alarm_time}:00.\n')
+def get_current_time():
+    while True:
+        try:
+            time_now = int(input('\nEnter the current time (00 - 23): '))
+            if time_now > 23 or time_now < 0:
+                print('The time must be between the hours 00 and 23.')
+            else:
+                return time_now
+        except ValueError:
+            print('Please enter a number.')
+
+def get_wait_time():
+    while True:
+        try: 
+            wait = int(input('\nEnter how many hours to wait for the alarm: '))
+            if wait < 0:
+                print('You cannot set an alarm in the past.')
+            else:
+                return wait
+        except ValueError: 
+            print('Please enter a number.')
+
 
 def calculate_alarm_time(time, duration):
     while True:
@@ -14,7 +33,20 @@ def calculate_alarm_time(time, duration):
                 time -= 24
         else:
             time += duration
-            return '00' if time == 24 else time
+            if time >= 24:
+                time -= 24
+            elif time == 24:
+                time == 0
+            return time
+
+def main():
+    current_time = get_current_time()
+    wait_time = get_wait_time()
+    alarm_time = calculate_alarm_time(current_time, wait_time)
+
+    print(f'\nCurrent time: {current_time}:00')
+    print(f'Alarm set {wait_time} hour(s) from now.')
+    print(f'Your alarm will ring at {alarm_time}:00.\n')
 
 if __name__ == '__main__':
     main()
