a0f0f754d866244793c3f2fbf14e96a8b67e64b5
Brady <brady@bradychin.dev>
2024-10-06 18:22:51 -0500

    CSC505 CT4 Update

--- Principles of Software Development/Critical Thinking/Critical Thinking 4/CriticalThinking4.py
+++ Principles of Software Development/Critical Thinking/Critical Thinking 4/CriticalThinking4.py
@@ -1,19 +1,28 @@
 import json
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
 
-personality_trait_file = 'Principles of Software Development/Critical Thinking/Critical Thinking 4/personalityTraits.json'
-important_steps = 'Principles of Software Development/Critical Thinking/Critical Thinking 4/importantSteps.json'
+# Get current folder path (where script is located).
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+# Define json files.
+personality_trait_file = 'personalityTraits.json'
+important_steps = 'importantSteps.json'
+
+# Open and read json files.
 with open(personality_trait_file) as file1:
     personality_data = json.load(file1)
 
 with open(important_steps) as file2:
     important_steps_data = json.load(file2)
 
+# Display personality traits.
 print(f'\nPersonality Traits:\n')
 for i, item in enumerate(personality_data):
     print(f'Trait {i+1}: {item['trait']}')
-    print(f'Description: {item['description']}\n')
+    print(f'Description: {''.join(item['description'])}\n')
 
+# Display important steps of the program.
 print(f'Important steps in the program:')
 for i, step in enumerate(important_steps_data):
     print(f'Step {step['step']}: {step['description']}')
