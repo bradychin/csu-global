26ec68b9dba54fc3b4966aa76d6ae2884858bc01
Brady <brady@bradychin.dev>
2025-02-02 13:48:44 -0600

    CSC510 Critical Thinking Complete

--- CSC510 Foundations of Artificial Intelligence/Critical Thinking Assignments/Critical Thinking 3/artificial_neural_network.py
+++ CSC510 Foundations of Artificial Intelligence/Critical Thinking Assignments/Critical Thinking 3/artificial_neural_network.py
@@ -1,25 +1,41 @@
+# 1. Import libraries
 import numpy as np
-# import tensorflow as tf
-import tf_keras
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
-# Input and Output data
+# 2. Input and Output data
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])  # Input layer
 y = np.array([[0], [1], [1], [0]])  # Output layer
 
-# Create the model
-model = tf_keras.Sequential([
-    tf_keras.layers.Dense(4, activation='relu', input_shape=(2,)),  # Hidden layer with input shape
-    tf_keras.layers.Dense(1, activation='sigmoid')  # Output layer
+# 3. Create the model
+# use sequential to stack layers
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(8, activation='relu', input_shape=(2,)),  # Hidden layer with input shape
+    tf.keras.layers.Dense(1, activation='sigmoid')  # Output layer
 ])
 
-# Compile the model
+# 4. Compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# Train the model
-model.fit(X, y, epochs=5000, verbose=0)
+# 5. Train the model (using all data for training)
+history = model.fit(X, y, epochs=5000, verbose=2)
 
-# Make predictions
+# 6. Make predictions
 predictions = model.predict(X)
 
-# Print predictions
+# 7. Print predictions
 print(f"Predictions: \n{predictions}")
+
+# 8. Plot the training loss values
+plt.plot(history.history['loss'])
+plt.title('Model Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.show()
+
+# 9. Plot the training accuracy values
+plt.plot(history.history['accuracy'])
+plt.title('Model Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.show()
