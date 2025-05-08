2d22dfbe4a112c93f06a022c7b85689394d6eb0d
Brady <brady@bradychin.dev>
2025-05-08 13:22:23 -0500

    CSC515 Portfolio Project Report Upload

--- CSC515 Foundations of Computer Vision /Portfolio Project/Portfolio Project/portfolio_project.py
+++ CSC515 Foundations of Computer Vision /Portfolio Project/Portfolio Project/portfolio_project.py
@@ -5,12 +5,11 @@ import matplotlib.pyplot as plt
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-
 def preprocess_image(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.equalizeHist(gray)
-    gray = cv2.bilateralFilter(gray, 9, 75, 75)
-    return gray
+    processed_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    processed_image = cv2.equalizeHist(processed_image)
+    processed_image = cv2.bilateralFilter(processed_image, 9, 75, 75)
+    return processed_image
 
 
 def detect_faces(img, gray):
@@ -21,7 +20,6 @@ def detect_faces(img, gray):
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
     return faces
-
 
 def align_and_blur_eyes(img, face_coords):
     for (x, y, w, h) in face_coords:
@@ -38,32 +36,34 @@ def align_and_blur_eyes(img, face_coords):
         img[y:y + h, x:x + w] = face
     return img
 
+def main():
+    image_paths = [
+        'images/full_body.jpg',
+        'images/front_facing.jpeg',
+        'images/front_facing_with_dog.jpg',
+        'images/multiple_people.jpg',
+        'images/person_far_away.jpg'
+    ]
 
-# List of your image paths
-image_paths = [
-    'images/full_body.jpg',
-    'images/front_facing.jpeg',
-    'images/front_facing_with_dog.jpg',
-    'images/multiple_people.jpg',
-    'images/person_far_away.jpg'
-]
+    results = []
+    titles = ['Full Body', 'Front Facing', 'Front Facing with Dog', 'Multiple People', 'Far Away']
 
-results = []
-titles = ['Full Body', 'Front Facing', 'Front Facing with Dog', 'Multiple People', 'Far Away']
+    for path in image_paths:
+        img = cv2.imread(path)
+        processed_image = preprocess_image(img)
+        faces = detect_faces(img, processed_image)
+        img = align_and_blur_eyes(img, faces)
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results.append(img_rgb)
 
-for path in image_paths:
-    img = cv2.imread(path)
-    gray = preprocess_image(img)
-    faces = detect_faces(img, gray)
-    img = align_and_blur_eyes(img, faces)
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results.append(img_rgb)
+    for i in range(5):
+        plt.subplot(2, 3, i+1)
+        plt.imshow(results[i])
+        plt.title(titles[i])
+        plt.axis('off')
 
-for i in range(5):
-    plt.subplot(2, 3, i+1)
-    plt.imshow(results[i])
-    plt.title(titles[i])
-    plt.axis('off')
+    plt.tight_layout()
+    plt.show()
 
-plt.tight_layout()
-plt.show()
+if __name__ == '__main__':
+    main()
